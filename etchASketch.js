192dce4a657a5c18aaf67098294986110b25751d
hueyp11 <118989093+hueyp11@users.noreply.github.com>
2023-05-25 22:25:22 -0400

    Created an user option for the number of available squares for the grid. Created default 25x25 grid at startup. Created function to clear grid. Still working towards making square divs for grid.

--- etchASketch.js
+++ etchASketch.js
@@ -1,20 +1,54 @@
 var mainBox = document.getElementById('main');
 var boxes = [];
+var resetUserInput = document.createElement('button');
+resetUserInput.innerText = 'RESET';
+document.body.appendChild(resetUserInput);
+resetUserInput.addEventListener('click', () => {
+    resetUserInput = prompt('Enter number of squares per side for the new grid (max 100 per side): ')
+    resetUserInput = resetUserInput ** 2;
+    if (resetUserInput > 10000) {
+        alert('Please enter a number less than 100!');
+    } else {
+        clearMainBox();
+        pushBoxes();
+        appendChildren(mainBox, boxes);
+    };
+});
 
 function createDiv () {
     var div = document.createElement('div');
     div.className = 'box';
-    div.style.width = "20px";
-    div.style.height = "20px";
-    div.addEventListener('mouseenter', () => {
+    //div.style.display = 'flex';
+    //div.style.justifyContent = 'center';
+    //div.style.alignItems = 'center';
+    div.style.flex = '1 0 4%';
+    div.style.width = `${400/100}px`;
+    //div.style.boxSizing = 'border-box';
+    // if (60 < resetUserInput <= 100) {
+    //     div.style.flex = '1 0 1%';
+    //     div.style.width = '1vh';
+    // } else if (0 < resetUserInput <= 60) {
+    //     div.style.flex = '1 0 3%';
+    // };
+    //div.style.height = `${400/25}px`;
+    //div.style.border = 'thin solid black';
+    div.addEventListener('mouseout', () => {
         div.style.backgroundColor = 'black'; 
     });
     return div;
 };
 
-for (let i=0; i < 256; i++) {
-    boxes.push(createDiv());
-}
+function pushBoxes () {
+    for (let i=0; i < resetUserInput; i++) {
+        boxes.push(createDiv());
+    };
+};
+
+function pushBoxesDefault () {
+    for (let i=0; i < 625; i++) {
+        boxes.push(createDiv());
+    };
+};
 
 function appendChildren (parent, children) {
     children.forEach(function (child) {
@@ -22,5 +56,12 @@ function appendChildren (parent, children) {
     });
 };
 
-appendChildren(mainBox, boxes);
+function clearMainBox () {
+    for (i=0; i < boxes.length; i++) {
+        boxes[i].style.display = 'none';
+    };
+};
 
+createDiv();
+pushBoxesDefault();
+appendChildren(mainBox, boxes);
